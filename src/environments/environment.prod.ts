de02739bede4089fb444f8bd35578a58cad4a3fc
AngelGamez2021 <agamez6@unisimon.edu.co>
2022-03-04 17:30:05 -0500

    avances

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBtF1Kr-aOgbCXh6s3BG21fF6NhilPSAhM",
+    authDomain: "empleados-5de90.firebaseapp.com",
+    projectId: "empleados-5de90",
+    storageBucket: "empleados-5de90.appspot.com",
+    messagingSenderId: "972259239286",
+    appId: "1:972259239286:web:87cf124f4ffa2c697ac657"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBtF1Kr-aOgbCXh6s3BG21fF6NhilPSAhM",
+    authDomain: "empleados-5de90.firebaseapp.com",
+    projectId: "empleados-5de90",
+    storageBucket: "empleados-5de90.appspot.com",
+    messagingSenderId: "972259239286",
+    appId: "1:972259239286:web:87cf124f4ffa2c697ac657"
+  }
 };
 
 /*
